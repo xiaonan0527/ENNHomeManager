16ae9e5fe1115bf3b2d608f60f21176e73470d26
xiaonan0527 <16151406@qq.com>
2016-03-08 09:29:48 +0800

    Update AppDelegate.m

--- ENNHomeManager/ENNHomeManager/Application/AppDelegate.m
+++ ENNHomeManager/ENNHomeManager/Application/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // test pull from github
     return YES;
 }
 
