241cba633448c6515d247ab1f85d72ba46e2e678
xiaonan <xiaonan@enn.cn>
2016-03-08 09:33:35 +0800

    add left tableviewcontroller

---
+++ ENNHomeManager/ENNHomeManager/Controllers/Main/LeftTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeftTableViewController.h
+//  ENNHomeManager
+//
+//  Created by xiaonan on 16/3/5.
+//  Copyright © 2016年 enn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftTableViewController : UITableViewController
+
+@end
---
+++ ENNHomeManager/ENNHomeManager/Controllers/Main/LeftTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  LeftTableViewController.m
+//  ENNHomeManager
+//
+//  Created by xiaonan on 16/3/5.
+//  Copyright © 2016年 enn. All rights reserved.
+//
+
+#import "LeftTableViewController.h"
+
+@implementation LeftTableViewController
+
+@end
